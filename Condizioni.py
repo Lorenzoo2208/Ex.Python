0b3fb787510952cc511f0238caa5f7c178bd59d8
Lorenzoo2208 <baiginilorenzo22@gmail.com>
2024-11-26 13:22:24 +0100

    Esercitazioni

---
+++ Condizioni.py
@@ -0,0 +1,13 @@
+#Esercitazione Condizioni
+
+import random
+
+if random > 10:
+    print("il numero è maggiore di 10.")
+elif random == 10:
+    print("il numero è uguale a 10.")
+else:
+    print("il numero è inferiore a 10.")    
+
+
+
--- Condizoni.py
+++
@@ -1,7 +0,0 @@
-#Esercitazione Condizioni
-
-
-numero1 = int(input("Inserisci il numero1:"))
-numero2 = int(input("Inserisci il numero2:"))
-numero3 = int(input("Inserisci il numero3:"))
-
